3b88b3bb5ecb22a38dd93493db85f4867a0e6b28
Fayyaz Alam <fayazalam.98@gmail.com>
2021-06-11 07:01:02 -0700

    js added to show live calculation display and entered numbers

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+// Clears Screen
+function clearScreen() {
+    document.getElementById("result").value = "";
+  }
+  // Displays entered value on screen.
+  function display(value) {
+    let res = document.getElementById("result");
+  
+    res.value += value;
+  }
