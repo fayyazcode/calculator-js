07a7614186b7e4fae1c723c8e139a07e70829e08
fayyazcode <fayazalam.98@gmail.com>
2021-06-15 11:11:20 +0500

    statment added on operators

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ function clearScreen() {
     document.getElementById("result").value = "";
   }
   // Displays entered value on screen.
-  function display(value) {
-    let res = document.getElementById("result");
+  function display(num) {
+    var res = document.getElementById("result");
+  var lastVal= res.value.slice(res.value.length - 1);
+  var operators=["+","-","/","*"];
+  if(operators.indexOf(lastVal) !== -1 && operators.indexOf(num) !== -1){
+    res.value = res.value.slice(0,res.value.length-1)+num
+  }
   
-    res.value += value;
+  else{
+    res.value += num;
+  }
+   
+  }
+  function removeOne(){
+    var res= document.getElementById('result');
+    res.value=res.value.slice(0,-1)
   }
